bb30c946661a9e258a9b831bd14e722b71076554
Brian Kwong <bkwong01@calpoly.edu>
2025-01-10 16:48:58 -0800

    point?

--- main.js
+++ main.js
@@ -1,23 +1,59 @@
 import './style.css';
-import {Map, View} from 'ol';
+import {Feature, Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import {toStringHDMS} from 'ol/coordinate.js';
+import {fromLonLat} from 'ol/proj';
+import {Point } from 'ol/geom';
+import { Vector } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import {Circle, Fill, Stroke, Style} from 'ol/style.js';
+import * as cities from "./cordinates.js"
 
 
-const slo = [35.30509299785206, -120.66261252029449] 
 const view = new View({
-  center: [0, 0],
-  zoom: 3
+  center: fromLonLat(cities.cal_poly),
+  zoom: 8 // Sets the default zoom level
 })
+const point = new Point(
+  fromLonLat(slo)
+)
 
-view.adjustCenter(slo)
+const fill = new Fill({
+  color: "#FF0000"
+});
+
+const stroke = new Stroke({
+  color: '#3399CC',
+  width: 1.25,
+});
+
+
+const point_layer = new Vector({
+  source: new VectorSource({
+    features: [ new Feature({
+      geometry: point
+    })]
+  }),
+  style : new Style({
+    image : new Circle({
+      fill : fill,
+      stroke : stroke,
+      radius : 25
+    }),
+    fill : fill,
+    stroke : stroke
+  })
+}
+)
+
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
-      source: new OSM()
+      source: new OSM(
+      )
     })
   ],
   view: view
 })
+map.addLayer(point_layer)
